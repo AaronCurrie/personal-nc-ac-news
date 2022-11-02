48c6e03713270c997e99d939d1e60ea5da239667
AaronCurrie <93119982+AaronCurrie@users.noreply.github.com>
2022-11-02 15:57:21 +0000

    sort the issue with the resetting the input box

--- src/Components/AddComment.jsx
+++ src/Components/AddComment.jsx
@@ -3,7 +3,7 @@ import { postNewComment } from '../utils/api'
 
 const AddComment = ({id, setComments}) => {
 
-    const [newComment, setNewComment] =useState(null)
+    const [newComment, setNewComment] =useState('')
     const [error, setError] = useState(false)
     const [userName, setUserName] = useState('tickle122')
 
@@ -15,22 +15,21 @@ const AddComment = ({id, setComments}) => {
     const handleSubmit = (event) => {
         event.preventDefault()
         if(error) {
-            console.log('comment')
+            
         } else {
             newComment? setError(false) : setError(true)
             postNewComment(id, newComment, userName).then(({comment}) => {
             setComments((currComments) => {
                 return currComments = [{author: userName, body: newComment, votes: 0, comment_id: Date.now()}, ...currComments]
             })
-            setNewComment(null)
+            setNewComment('')
             })
         }
-
     }
 
     return (
         <form onSubmit={(event) => handleSubmit(event)} className='flex-row'>
-            <textarea onChange={(event) => handleChange(event)} className={error? 'add-comment error' : "add-comment"} aria-label='Add new comment' id='newComment' name='newComment' rows='4' cols='50' placeholder="Add a Comment..."></textarea>
+            <textarea value={newComment} onChange={(event) => handleChange(event)} className={error? 'add-comment error' : "add-comment"} aria-label='Add new comment' id='newComment' name='newComment' rows='4' cols='50' placeholder="Add a Comment..."></textarea>
             <button type='submit' className='load-button'>Post</button>
         </form>
     )
