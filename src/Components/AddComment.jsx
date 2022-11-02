ba672389b4b25f51c5e9d0f4bdc427d9ae318dc2
AaronCurrie <93119982+AaronCurrie@users.noreply.github.com>
2022-11-02 16:26:47 +0000

    fixed bug to post empty comments

--- src/Components/AddComment.jsx
+++ src/Components/AddComment.jsx
@@ -9,28 +9,35 @@ const AddComment = ({id, setComments}) => {
 
     const handleChange = (event) => {
         setNewComment(event.target.value)
-        setError(false)
+        if(event.target.value === '') {
+            setError(true)
+        } else {
+            setError(false)
+            console.log(newComment)
+        }
+
     }
 
     const handleSubmit = (event) => {
+        newComment === ''? setError(true) : setError(false)
         event.preventDefault()
         if(error) {
-            
+
         } else {
-            newComment? setError(false) : setError(true)
-            postNewComment(id, newComment, userName).then(({comment}) => {
             setComments((currComments) => {
-                return currComments = [{author: userName, body: newComment, votes: 0, comment_id: Date.now()}, ...currComments]
+                return [{author: userName, body: newComment, votes: 0, comment_id: Date.now()}, ...currComments]
             })
             setNewComment('')
+            postNewComment(id, newComment, userName).then(({comment}) => {   
+
             })
         }
     }
 
     return (
         <form onSubmit={(event) => handleSubmit(event)} className='flex-row'>
-            <textarea value={newComment} onChange={(event) => handleChange(event)} className={error? 'add-comment error' : "add-comment"} aria-label='Add new comment' id='newComment' name='newComment' rows='4' cols='50' placeholder="Add a Comment..."></textarea>
-            <button type='submit' className='load-button'>Post</button>
+            <textarea value={newComment} onChange={(event) => handleChange(event)} className={error?'error add-comment' : 'add-comment'} aria-label='Add new comment' id='newComment' name='newComment' rows='4' cols='50' placeholder="Add a Comment..."></textarea>
+            <button type='submit' disabled={newComment===''} className='load-button'>Post</button>
         </form>
     )
 }
