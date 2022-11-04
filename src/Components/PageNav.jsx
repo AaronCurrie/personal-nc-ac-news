c7a7622ad7cc3faa55e2f16ef004034f43a4c010
AaronCurrie <93119982+AaronCurrie@users.noreply.github.com>
2022-11-04 15:22:30 +0000

    tweaks

--- src/Components/PageNav.jsx
+++ src/Components/PageNav.jsx
@@ -1,14 +1,21 @@
 import { useEffect, useState } from "react"
-import { Link } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 
 const PageNav = ({noOfPages, p=1, sort='created_at', order='DESC'}) => {
-
+    const {topic} = useParams()
     const [pagesArr, setPagesArr] = useState([])
     const [isLoading, setLoading] = useState(true)
+    const [forcetopic, setTopic] = useState(topic)
+    
 
 
     useEffect(() => {
         setLoading(true)
+        if(topic === undefined) {
+            setTopic(() => 'all')
+        } else {
+            setTopic(() => '')
+        }
         setPagesArr(Array.from({length: noOfPages}, (_, i) => i + 1)) 
         setLoading(false)
     }, [noOfPages])
@@ -19,7 +26,7 @@ const PageNav = ({noOfPages, p=1, sort='created_at', order='DESC'}) => {
         <nav className="page-nav flex-row">
             <p>Page:</p>
             {pagesArr.map(page => {
-                return <Link className={page == p? `active`:``} to={`?p=${page}&sort_by=${sort}&order=${order}`} key={page}>{page}</Link>
+                return <Link className={page == p? `active`:``} to={`${forcetopic}?p=${page}&sort_by=${sort}&order=${order}`} key={page}>{page}</Link>
             })}
         </nav>
     )
